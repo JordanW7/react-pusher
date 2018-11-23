68755555171a334ac655c72f2bb934216bc58c84
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-11-23 10:13:00 +0000

    adding background and changing avatar element

--- src/components/CommentEntry.js
+++ src/components/CommentEntry.js
@@ -4,11 +4,16 @@ import './CommentEntry.css';
 
 const CommentEntry = () => {
 	return (
-		<Row>
-			<div>
-			<Col span={24} className="commententry-user">
-				<Avatar className="commententry-avatar">A</Avatar>
+		<Row className="commententry">
+		<div className="commententry-user">
+			<Col span={18}>
+				{//<Avatar className="commententry-avatar">A</Avatar>
+				}
+				<img src="http://placeimg.com/40/40/people" className="commententry-avatar" alt="Anthony Smith" />
 				Anthony Smith says:
+			</Col>
+			<Col span={6} className="commententry-user">
+			(10sec ago)
 			</Col>
 			</div>
 			<Col span={24}>
