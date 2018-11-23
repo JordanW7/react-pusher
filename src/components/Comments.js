0dcbfe1c69ceb1fca0a7243665564e11a90f33e9
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-11-23 11:48:59 +0000

    connecting socket

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,43 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Row, Col, Input, Button } from "antd";
-import CommentEntry from './CommentEntry';
-import './Comments.css';
+import CommentEntry from "./CommentEntry";
+import { subscribeToTimer } from "../api/api";
+import "./Comments.css";
 
 class Comments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timestamp: "no timestamp yet"
+    };
+    subscribeToTimer((err, timestamp) =>
+      this.setState({
+        timestamp
+      })
+    );
+  }
   render() {
+    console.log("STAMP", this.state.timestamp);
     return (
       <section className="comments">
-	      <div className="comments-current">
-		      <CommentEntry />
-		      <CommentEntry />
-		      <CommentEntry />
-		      <CommentEntry />
-		      <CommentEntry />
-		  </div>
-		  <Row>
-		  <Col
-              xs={21}
-              sm={21}
-              md={21}
-              lg={18}
-              xl={18}
+        <div className="comments-current">
+          <CommentEntry />
+          <CommentEntry />
+          <CommentEntry />
+          <CommentEntry />
+          <CommentEntry />
+        </div>
+        <Row>
+          <Col xs={21} sm={21} md={21} lg={18} xl={18}>
+            <Input className="comments-input" />
+          </Col>
+          <Col xs={3} sm={3} md={3} lg={6} xl={6}>
+            <Button
+              type="primary"
+              onClick={this.handleSubmit}
+              className="comments-button"
             >
-		  <Input className="comments-input"/>
-		 </Col>
-		 <Col
-              xs={3}
-              sm={3}
-              md={3}
-              lg={6}
-              xl={6}
-            >
-		  	<Button type="primary" onClick={this.handleSubmit} className="comments-button">Add</Button>
-		  	</Col>
-		  	</Row>
+              Add
+            </Button>
+          </Col>
+        </Row>
       </section>
     );
   }
 }
 
 export default Comments;
-
